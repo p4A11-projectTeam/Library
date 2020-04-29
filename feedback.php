9251dc19912ef9cb86491bc3f650b8b9f4362592
p4A11-projectTeam <p4A11-projectTeam>
2020-04-29 23:45:08 +0530

    no change

--- feedback.php
+++ feedback.php
@@ -22,7 +22,7 @@
    $body = "A user has entered feedback on the site!\n";
    $body .= "Their feedback is:\n\n";
    $body .= $_POST['message'];
-   $headers="From: lms"
+   $headers="From: lms";
    mail($to, $subject, $body,$headers);
    mysqli_query($db,"INSERT INTO `feedback`(`name`, `email`, `subject`, `message`) VALUES('$_POST[name]','$_POST[email]','$_POST[subject]','$_POST[message]');");
    print("<h2>Thanks for your feedback!</h2>");
